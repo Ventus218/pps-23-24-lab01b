d9a30c576efd50036ad0e94802eba1781e3b1a2e
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-05 19:03:01 +0100

    added tests for classic strategy

--- src/test/java/e1/ClassicLogicStrategyTest.java
+++ src/test/java/e1/ClassicLogicStrategyTest.java
@@ -42,4 +42,37 @@ public class ClassicLogicStrategyTest {
                     return () -> assertTrue(strategy.canMove(currentPosition, destination));
                 }));
     }
+
+    @Test
+    void cannotMoveToNotAllowedDestinations() {
+        Pair<Integer, Integer> currentPosition = new Pair<>(2, 2);
+        List<Pair<Integer, Integer>> allowedDestinations = new ArrayList<>();
+        allowedDestinations.add(new Pair<>(0, 1));
+        allowedDestinations.add(new Pair<>(0, 3));
+        allowedDestinations.add(new Pair<>(1, 0));
+        allowedDestinations.add(new Pair<>(1, 4));
+        allowedDestinations.add(new Pair<>(3, 0));
+        allowedDestinations.add(new Pair<>(3, 4));
+        allowedDestinations.add(new Pair<>(4, 1));
+        allowedDestinations.add(new Pair<>(4, 3));
+
+        // Generating every non-allowed destination is impossible due to the fact that
+        // the strategy doesn't make any assumption on the board size.
+        var size = 5;
+        List<Pair<Integer, Integer>> notAllowedDestinations = new ArrayList<>();
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                var destination = new Pair<>(x, y);
+                if (!allowedDestinations.contains(destination)) {
+                    notAllowedDestinations.add(destination);
+                }
+            }
+        }
+
+        assertAll(notAllowedDestinations
+                .stream()
+                .map(destination -> {
+                    return () -> assertFalse(strategy.canMove(currentPosition, destination));
+                }));
+    }
 }
