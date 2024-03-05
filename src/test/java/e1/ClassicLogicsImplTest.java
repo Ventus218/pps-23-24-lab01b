2f773be29e97b6fb278193613ed18385c1724f61
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-05 19:31:23 +0100

    Test refactor
    Made LogicTest an abstract test class which can be extended in order to test Logics with different implementation but common base behaviour

---
+++ src/test/java/e1/ClassicLogicsImplTest.java
@@ -0,0 +1,85 @@
+package e1;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ClassicLogicsImplTest extends LogicTest {
+
+    @Override
+    int boardSize() {
+        return 5;
+    }
+
+    @Override
+    Logics makeLogics() {
+        return new LogicsImpl(boardSize(), new ClassicLogicStrategy());
+    }
+
+    @Test
+    void knightHitsPawn() {
+        var pawnPosition = new Pair<Integer, Integer>(0, 0);
+        var knightPosition = new Pair<Integer, Integer>(1, 2);
+        logics = new LogicsImpl(boardSize(), new ClassicLogicStrategy(), knightPosition, pawnPosition);
+
+        assertAll(
+                () -> assertTrue(logics.hit(pawnPosition.getX(), pawnPosition.getY())),
+                () -> assertTrue(logics.hasKnight(pawnPosition.getX(), pawnPosition.getY())));
+    }
+
+    @Test
+    void knightMissesPawn() {
+        var pawnPosition = new Pair<Integer, Integer>(0, 0);
+        var knightPosition = new Pair<Integer, Integer>(1, 2);
+        var hitPosition = new Pair<Integer, Integer>(2, 0);
+        logics = new LogicsImpl(boardSize(), new ClassicLogicStrategy(), knightPosition, pawnPosition);
+
+        assertAll(
+                () -> assertFalse(logics.hit(hitPosition.getX(), hitPosition.getY())),
+                () -> assertTrue(logics.hasKnight(hitPosition.getX(), hitPosition.getY())));
+    }
+
+    @Test
+    void knightMovementPatternIsCorrect() {
+        var boardSize = 5;
+        var initialKnightPosition = new Pair<>(2, 2);
+        var pawnPosition = new Pair<>(0, 0);
+
+        // T indicates the positions where the knight is allowed to move.
+        // Moving to any other position is not allowed.
+        //
+        // In order to test this behaviour the pawn must be placed in a position where
+        // the knight can't move
+        //
+        // .. 0 1 2 3 4
+        // 0 | |T| |T| |
+        // 1 |T| | | |T|
+        // 2 | | |K| | |
+        // 3 |T| | | |T|
+        // 4 | |T| |T| |
+
+        var allowedPositions = resultForEachPosition(position -> {
+            return position.equals(new Pair<>(0, 1)) ||
+                    position.equals(new Pair<>(0, 3)) ||
+                    position.equals(new Pair<>(1, 0)) ||
+                    position.equals(new Pair<>(1, 4)) ||
+                    position.equals(new Pair<>(3, 0)) ||
+                    position.equals(new Pair<>(3, 4)) ||
+                    position.equals(new Pair<>(4, 1)) ||
+                    position.equals(new Pair<>(4, 3));
+        }, boardSize);
+
+        var actualPositions = resultForEachPosition(position -> {
+            logics = new LogicsImpl(boardSize, new ClassicLogicStrategy(), initialKnightPosition, pawnPosition);
+            var x = position.getX();
+            var y = position.getY();
+            logics.hit(x, y);
+            return logics.hasKnight(x, y) && !position.equals(initialKnightPosition);
+        }, boardSize);
+
+        assertEquals(allowedPositions, actualPositions);
+    }
+}
--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -5,87 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Function;
 
-public class LogicTest {
+public abstract class LogicTest {
 
-  private Logics logics;
-  private static final int SIZE = 3;
+  protected Logics logics;
+
+  abstract int boardSize();
+
+  abstract Logics makeLogics();
 
   @BeforeEach
   void init() {
-    logics = new LogicsImpl(SIZE, new ClassicLogicStrategy());
-  }
-
-  @Test
-  void knightHitsPawn() {
-    var pawnPosition = new Pair<Integer, Integer>(0, 0);
-    var knightPosition = new Pair<Integer, Integer>(1, 2);
-    logics = new LogicsImpl(SIZE, new ClassicLogicStrategy(), knightPosition, pawnPosition);
-
-    assertAll(
-        () -> assertTrue(logics.hit(pawnPosition.getX(), pawnPosition.getY())),
-        () -> assertTrue(logics.hasKnight(pawnPosition.getX(), pawnPosition.getY())));
-  }
-
-  @Test
-  void knightMissesPawn() {
-    var pawnPosition = new Pair<Integer, Integer>(0, 0);
-    var knightPosition = new Pair<Integer, Integer>(1, 2);
-    var hitPosition = new Pair<Integer, Integer>(2, 0);
-    logics = new LogicsImpl(SIZE, new ClassicLogicStrategy(), knightPosition, pawnPosition);
-
-    assertAll(
-        () -> assertFalse(logics.hit(hitPosition.getX(), hitPosition.getY())),
-        () -> assertTrue(logics.hasKnight(hitPosition.getX(), hitPosition.getY())));
-  }
-
-  @Test
-  void knightMovementPatternIsCorrect() {
-    var boardSize = 5;
-    var initialKnightPosition = new Pair<>(2, 2);
-    var pawnPosition = new Pair<>(0, 0);
-
-    // T indicates the positions where the knight is allowed to move.
-    // Moving to any other position is not allowed.
-    //
-    // In order to test this behaviour the pawn must be placed in a position where
-    // the knight can't move
-    //
-    // .. 0 1 2 3 4
-    // 0 | |T| |T| |
-    // 1 |T| | | |T|
-    // 2 | | |K| | |
-    // 3 |T| | | |T|
-    // 4 | |T| |T| |
-
-    var allowedPositions = resultForEachPosition(position -> {
-      return position.equals(new Pair<>(0, 1)) ||
-          position.equals(new Pair<>(0, 3)) ||
-          position.equals(new Pair<>(1, 0)) ||
-          position.equals(new Pair<>(1, 4)) ||
-          position.equals(new Pair<>(3, 0)) ||
-          position.equals(new Pair<>(3, 4)) ||
-          position.equals(new Pair<>(4, 1)) ||
-          position.equals(new Pair<>(4, 3));
-    }, boardSize);
-
-    var actualPositions = resultForEachPosition(position -> {
-      logics = new LogicsImpl(boardSize, new ClassicLogicStrategy(), initialKnightPosition, pawnPosition);
-      var x = position.getX();
-      var y = position.getY();
-      logics.hit(x, y);
-      return logics.hasKnight(x, y) && !position.equals(initialKnightPosition);
-    }, boardSize);
-
-    assertEquals(allowedPositions, actualPositions);
+    logics = makeLogics();
   }
 
   @Test
   void hitThrowsWhenPositionIsOutOfBounds() {
-    assertAll(() -> assertThrows(IndexOutOfBoundsException.class, () -> logics.hit(0, SIZE)),
-        () -> assertThrows(IndexOutOfBoundsException.class, () -> logics.hit(SIZE, 0)),
+    assertAll(() -> assertThrows(IndexOutOfBoundsException.class, () -> logics.hit(0, boardSize())),
+        () -> assertThrows(IndexOutOfBoundsException.class, () -> logics.hit(boardSize(), 0)),
         () -> assertThrows(IndexOutOfBoundsException.class, () -> logics.hit(0, -1)),
         () -> assertThrows(IndexOutOfBoundsException.class, () -> logics.hit(-1, 0)));
   }
@@ -124,14 +62,14 @@ public class LogicTest {
   /**
    * Returns a mapping from every position of the board to the result of the given
    * function for that position.
-   * Uses the test class SIZE
+   * Uses the test class boardSize()
    * 
    * @param <T>
    * @param function
    * @return
    */
   <T> Map<Pair<Integer, Integer>, T> resultForEachPosition(Function<Pair<Integer, Integer>, T> function) {
-    return resultForEachPosition(function, SIZE);
+    return resultForEachPosition(function, boardSize());
   }
 
   /**
