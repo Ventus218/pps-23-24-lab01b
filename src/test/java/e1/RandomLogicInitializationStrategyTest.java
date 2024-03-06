d1cd9d63d0c4851e2f9b67887163aaa1cf0ee383
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-06 11:01:13 +0100

    Allowing test class to be inherited

--- src/test/java/e1/RandomLogicInitializationStrategyTest.java
+++ src/test/java/e1/RandomLogicInitializationStrategyTest.java
@@ -2,28 +2,39 @@ package e1;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RandomLogicInitializationStrategyTest {
 
+    protected static final int SEED = 1234;
+
+    // In order to test randomicity may be a good idea to make many calls and test
+    // that at least one of them is different from all the others.
+    // Also using a large boardSize reduces the probability of getting the same
+    // initialization multiple times.
+    protected static final int BOARD_SIZE = 8;
+    protected static final int NUMBER_OF_SUBSEQUENT_CALLS = 10;
+
+    protected LogicInitializationStrategy strategy;
+
+    protected LogicInitializationStrategy makeLogicInitializationStrategy() {
+        return new RandomLogicInitializationStrategy(SEED);
+    }
+
+    @BeforeEach
+    void init() {
+        strategy = makeLogicInitializationStrategy();
+    }
+
     @Test
     void subsequentInitializationsReturnDifferentInitializationConfigurations() {
-        // In order to test randomicity may be a good idea to make many calls and test
-        // that at least one of them is different from all the others.
-        // Also using a large boardSize reduces the probability of getting the same
-        // initialization multiple times.
-
-        var seed = 1234;
-        int boardSize = 8;
-        int calls = 10;
-        LogicInitializationStrategy strategy = new RandomLogicInitializationStrategy(seed);
-
-        LogicInitializationConfiguration configuration = strategy.initializeConfiguration(boardSize);
-        for (int i = 0; i < calls; i++) {
-            if (!configuration.equals(strategy.initializeConfiguration(boardSize))) {
+        LogicInitializationConfiguration configuration = strategy.initializeConfiguration(BOARD_SIZE);
+        for (int i = 0; i < NUMBER_OF_SUBSEQUENT_CALLS; i++) {
+            if (!configuration.equals(strategy.initializeConfiguration(BOARD_SIZE))) {
                 return;
             }
         }
-        fail(calls + " subsequent calls returned always the same configuration");
+        fail(NUMBER_OF_SUBSEQUENT_CALLS + " subsequent calls returned always the same configuration");
     }
 }
